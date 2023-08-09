5d7de0b5fb5543fc5cdfa3f93b263bb225db19e0
Luis Martinez <luis.hernando.martinez@correounivalle.edu.co>
2023-08-09 09:40:22 -0500

    python scripts

--- Arduino scripts/Simu_sens/Simu_sens.ino
+++ Arduino scripts/Simu_sens/Simu_sens.ino
@@ -1,26 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <SoftwareSerial.h>
 
-float ruido;
-float temp;
-float nluz;
-float pot;
+#define RESULT_BUFFER_SIZE (8)
+SoftwareSerial BT(2,3);
 
-void setup(){
+//uint8_t
+ float db   = 0;
+ float temp = 0;
+ float lux  = 0;
+ float pot  = 0;
+
+//char result_buf[RESULT_BUFFER_SIZE];
+
+void setup() {
   Serial.begin(9600);    // Iniciamos la puerta serie
+  BT.begin(9600);
 }
 
-void loop(){
-  ruido = 20 * log10(analogRead(A5)*5/1024) ;
-  temp = analogRead(A0)*5000*10/ 1024;
-  nluz = analogRead(A1)/4;
-  pot = analogRead(A2)*250000/1024;
-
-  Serial.print("Temperature(ºC): ");
-  Serial.println(temp);
-  Serial.print("Ruido(dB): ");
-  Serial.println(ruido);
-  Serial.print("Resistencia(Ω): ");
-  Serial.println(pot);
-  Serial.print("Nivel de luz: ");
-  Serial.println(nluz);
-  delay(2500);
+void loop() {
+  db    = analogRead(A5);
+  temp  = (analogRead(A0) * 5000 * 10 / 1024);
+  lux   = (analogRead(A1) / 4);
+  pot   = (analogRead(A2) * 250000 / 1024);
+  //char s [80];
+  //sprintf (s,"{\"temp\": %f, \"lux\": %f, \"noice\": %f,\"ohm\": %f}", temp, lux, db, pot);
+  //Serial.println (s);
+  Serial.print("{\"temp\": ");
+  Serial.print(temp,0);
+  Serial.print(",");
+  Serial.print(" \"lux\": ");
+  Serial.print(lux,0);
+  Serial.print(",");
+  Serial.print(" \"noise\": ");
+  Serial.print(db,2);
+  Serial.print(",");
+  Serial.print(" \"ohm\": ");
+  Serial.print(pot,0);
+  Serial.println("}");
+  
+  delay(1000);
 }
--- Arduino scripts/sketch_mar27a/sketch_mar27a2.ino/sketch_mar27a2.ino.ino
+++
@@ -1,9 +0,0 @@
-void setup(){
-  Serial.begin(9600);    // Iniciamos la puerta serie
-}
-
-void loop(){
-  float Lectura = analogRead(A5) ;
-  Serial.println(Lectura);
-  delay(200);
-}
